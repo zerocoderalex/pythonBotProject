e5ca6035caec621f2a0779a2b31c0efdbd9a6915
zerocoderalex <alex12449@mail.ru>
2024-12-02 23:14:57 +0500

    add func

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import asyncio
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message(F.foto)
+async def start(message: Message):
+    await message.answer('Классная фотка!')
+
+@dp.message(F.text =='Кто ты?')
+async def start(message: Message):
+    await message.answer('Я искусственный интеллект.')
+
+@dp.message(Command('help'))
+async def start(message: Message):
+    await message.answer("Я умею выполнять команды:\n/start\n/help")
 
 @dp.message(CommandStart())
 async def start(message: Message):
