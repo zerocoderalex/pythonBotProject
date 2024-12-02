e4e8f83ce32d323705519dabcc37e3802c9d88f6
zerocoderalex <alex12449@mail.ru>
2024-12-02 23:28:51 +0500

    correct

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
-
+import random
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(F.foto)
-async def start(message: Message):
-    await message.answer('Классная фотка!')
+@dp.message(F.photo)
+async def answ_photo(message: Message):
+    spis = ['Ого, какая фотка!', 'Что это такое?', 'Не отправляй мне такое больше']
+    rand_answ = random.choice(spis)
+    await message.answer(rand_answ)
 
 @dp.message(F.text =='Кто ты?')
-async def start(message: Message):
+async def answ_mes(message: Message):
     await message.answer('Я искусственный интеллект.')
 
 @dp.message(Command('help'))
-async def start(message: Message):
+async def helps(message: Message):
     await message.answer("Я умею выполнять команды:\n/start\n/help")
 
 @dp.message(CommandStart())
