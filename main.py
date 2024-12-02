5a96b652fbba24fa3823722360f64e338e3ff7a1
zerocoderalex <alex12449@mail.ru>
2024-12-02 22:06:09 +0500

    add def

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from config import TOKEN
 
 
-bot = Bot(token='')
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("Привет, я бот!")
+
 async def main():
-    await dp.start_polling()
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
