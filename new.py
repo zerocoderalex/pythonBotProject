915515a58a5b363b5db463f7730122c53502a2af
zerocoderalex <alex12449@mail.ru>
2024-12-06 18:53:07 +0500

    create db

--- new.py
+++ new.py
@@ -33,8 +33,39 @@ def init_db():
 	''')
     conn.commit()
     conn.close()
+
 init_db()
 
+@dp.message(CommandStart())
+async def start(message: Message, state: FSMContext):
+    await message.answer("Привет! Как тебя зовут?")
+    await state.set_state(Form.name)
+
+@dp.message(Form.name)
+async def name(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.answer("Сколько тебе лет?")
+    await state.set_state(Form.age)
+
+@dp.message(Form.age)
+async def age(message: Message, state: FSMContext):
+    await state.update_data(age=message.text)
+    await message.answer("Из какого ты города?")
+    await state.set_state(Form.city)
+
+@dp.message(Form.city)
+async def city(message: Message, state:FSMContext):
+    await state.update_data(city=message.text)
+
+    user_data = await state.get_data()
+
+    conn = sqlite3.connect('user_data.db')
+    cur = conn.cursor()
+    cur.execute('''
+       INSERT INTO users (name, age, city) VALUES (?, ?, ?)''',
+                (user_data['name'], user_data['age'], user_data['city']))
+    conn.commit()
+    conn.close()
 
 
 
